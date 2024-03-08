5af8e92b0ece82b70150f046b580cac450c4c25f
brent <steyn7788@gmail.com>
2024-03-08 20:49:16 +0200

    Started with the win enter event and on save event

--- plugin/nvim_lang.lua
+++ plugin/nvim_lang.lua
@@ -1,1 +1,31 @@
 vim.notify("Hello From Nvim Lang")
+
+
+local _ = vim.fn.getcwd()
+
+local buf_info = vim.fn.getbufinfo()
+
+-- vim.print(buf_info)
+
+vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+	group = vim.api.nvim_create_augroup('nvim_lang_on_window_enter', { clear = true }),
+	callback = function(arg)
+		if arg == nil or arg.file == nil or arg.file == "" then
+			return
+		end
+		vim.print("On WIN enter")
+		vim.print(arg)
+	end
+})
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+	group = vim.api.nvim_create_augroup('nvim_lang_on_save', { clear = true }),
+	callback = function(arg)
+		if arg == nil or arg.file == nil or arg.file == "" then
+			return
+		end
+
+		vim.print("On save")
+		vim.print(arg)
+	end
+})
