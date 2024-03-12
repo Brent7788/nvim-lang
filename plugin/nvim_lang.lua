325ad82b6f37ad5aa21558bfd6db27098270eed2
brent <steyn7788@gmail.com>
2024-03-12 20:21:12 +0200

    Started with the underline highlight code

--- plugin/nvim_lang.lua
+++ plugin/nvim_lang.lua
@@ -32,6 +32,36 @@ local function isNotValidNvimLangFile(arg)
 	return false
 end
 
+local function apply_underline(nvim_lang_file)
+	-- pub struct NvimLanguageLine {
+	--     pub line_number: usize,
+	--     pub start_column: usize,
+	--     pub end_column: usize,
+	--     pub options: NvimOptions,
+	--     pub data_type: NvimLangLineType,
+	-- }
+
+	vim.cmd(
+		':highlight default MyUnderline guisp=#EB5757 gui=undercurl ctermfg=198 cterm=undercurl')
+
+	for _, nll in pairs(nvim_lang_file.nvim_lang_lines) do
+		vim.print(nll)
+		local namespace_id = vim.api.nvim_create_namespace('MyNamespace')
+		-- TODO: Need to get buffer id from nvim_lang_file path
+		local buffer_id = vim.api.nvim_get_current_buf()
+		local line_number = nll.line_number
+		local end_col = nll.end_column
+		local start_col = nll.start_column
+		vim.api.nvim_buf_set_extmark(
+			buffer_id, namespace_id, line_number - 1, start_col,
+			{
+				end_row = line_number - 1,
+				end_col = end_col,
+				hl_group = 'MyUnderline'
+			})
+	end
+end
+
 vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
 	group = vim.api.nvim_create_augroup('nvim_lang_on_window_enter', { clear = true }),
 	callback = function(arg)
@@ -41,6 +71,7 @@ vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
 		vim.print("On WIN enter")
 		vim.print(arg)
 		local file = arg.file
+		local timeout = 6000;
 
 		main.start_processing(file);
 
@@ -50,10 +81,19 @@ vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
 			local n = main.check_process()
 
 			if n and next(n.nvim_lang_lines) then
-				vim.print(n)
+				apply_underline(n)
 				timer:stop();
 				return
 			end
+
+			-- INFO: Stop after one minute
+			if timeout <= 0 then
+				vim.notify("Nvim Lang Timeout!")
+				timer:stop();
+			end
+
+			timeout = timeout - 100;
+
 			vim.print(n)
 		end))
 
