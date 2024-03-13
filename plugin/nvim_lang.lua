2dfae09a8339778bea9e8413337a12bdfd2883b4
brent <steyn7788@gmail.com>
2024-03-13 20:25:42 +0200

    Abstraction and more highlight

--- plugin/nvim_lang.lua
+++ plugin/nvim_lang.lua
@@ -3,6 +3,15 @@ vim.print("Nvim Lang was loaded")
 -- TODO: Remove. This is only here for testing.
 vim.opt.runtimepath:append("~/Documents/projects/nvim-lang-core/target/release")
 
+vim.cmd(
+	':highlight default NvimLanguageTypo guisp=#EB5757 gui=undercurl ctermfg=198 cterm=undercurl')
+
+vim.cmd(
+	':highlight default NvimLanguageGrammar guisp=#F2B24C gui=undercurl ctermfg=198 cterm=undercurl')
+
+vim.cmd(
+	':highlight default NvimLanguageMisc guisp=#8F7FFF gui=undercurl ctermfg=198 cterm=undercurl')
+
 local main = require("main")
 vim.print(main)
 
@@ -32,7 +41,9 @@ local function isNotValidNvimLangFile(arg)
 	return false
 end
 
-local function apply_underline(nvim_lang_file)
+local namespace_id = vim.api.nvim_create_namespace('NvimLanguageNamespace')
+
+local function apply_underline(nvim_lang_file, current_buffer_id)
 	-- pub struct NvimLanguageLine {
 	--     pub line_number: usize,
 	--     pub start_column: usize,
@@ -41,25 +52,80 @@ local function apply_underline(nvim_lang_file)
 	--     pub data_type: NvimLangLineType,
 	-- }
 
-	vim.cmd(
-		':highlight default MyUnderline guisp=#EB5757 gui=undercurl ctermfg=198 cterm=undercurl')
+	-- Typos,
+	-- Punctuation,
+	-- ConfusedWords,
+	-- Redundancy,
+	-- Casing,
+	-- Grammar,
+	-- Misc,
+	-- Semantics,
+	-- Typograph,
+	-- Other,
+
 
 	for _, nll in pairs(nvim_lang_file.nvim_lang_lines) do
 		vim.print(nll)
-		local namespace_id = vim.api.nvim_create_namespace('MyNamespace')
-		-- TODO: Need to get buffer id from nvim_lang_file path
-		local buffer_id = vim.api.nvim_get_current_buf()
+		local buffer_id = current_buffer_id
 		local line_number = nll.line_number
 		local end_col = nll.end_column
 		local start_col = nll.start_column
-		vim.api.nvim_buf_set_extmark(
-			buffer_id, namespace_id, line_number - 1, start_col,
-			{
-				end_row = line_number - 1,
-				end_col = end_col,
-				hl_group = 'MyUnderline'
-			})
+
+		if nll.data_type == "Typos" then
+			vim.api.nvim_buf_set_extmark(
+				buffer_id, namespace_id, line_number - 1, start_col,
+				{
+					end_row = line_number - 1,
+					end_col = end_col,
+					hl_group = 'NvimLanguageTypo'
+				})
+		elseif nll.data_type == "Redundancy" then
+			vim.api.nvim_buf_set_extmark(
+				buffer_id, namespace_id, line_number - 1, start_col,
+				{
+					end_row = line_number - 1,
+					end_col = end_col,
+					hl_group = 'NvimLanguageMisc'
+				})
+		else
+			vim.api.nvim_buf_set_extmark(
+				buffer_id, namespace_id, line_number - 1, start_col,
+				{
+					end_row = line_number - 1,
+					end_col = end_col,
+					hl_group = 'NvimLanguageGrammar'
+				})
+		end
 	end
+end
+
+local function process(arg)
+	local file = arg.file
+	local timeout = 30000;
+
+	main.start_processing(file);
+
+	local timer = vim.loop.new_timer();
+
+	timer:start(100, 100, vim.schedule_wrap(function()
+		local nvim_lang_file = main.check_process()
+
+		if nvim_lang_file and next(nvim_lang_file.nvim_lang_lines) then
+			apply_underline(nvim_lang_file, arg.buf)
+			timer:stop();
+			return
+		end
+
+		-- INFO: Stop after 30 sec
+		if timeout <= 0 then
+			vim.notify("Nvim Lang Timeout! On " .. file)
+			timer:stop();
+		end
+
+		timeout = timeout - 100;
+
+		vim.print(nvim_lang_file)
+	end))
 end
 
 vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
@@ -70,33 +136,7 @@ vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
 		end
 		vim.print("On WIN enter")
 		vim.print(arg)
-		local file = arg.file
-		local timeout = 6000;
-
-		main.start_processing(file);
-
-		local timer = vim.loop.new_timer();
-
-		timer:start(100, 100, vim.schedule_wrap(function()
-			local n = main.check_process()
-
-			if n and next(n.nvim_lang_lines) then
-				apply_underline(n)
-				timer:stop();
-				return
-			end
-
-			-- INFO: Stop after one minute
-			if timeout <= 0 then
-				vim.notify("Nvim Lang Timeout!")
-				timer:stop();
-			end
-
-			timeout = timeout - 100;
-
-			vim.print(n)
-		end))
-
+		process(arg)
 		vim.print("End of WIN Enter")
 	end
 })
@@ -110,5 +150,6 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 
 		vim.print("On save")
 		vim.print(arg)
+		process(arg)
 	end
 })
